a2a6532ae131d8b48ead2a9b17c31b53628dbfc9
ggomaeng <sungwoopark95@gmail.com>
2017-02-27 19:31:53 +0900

    webhook

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ firebase.initializeApp(config.FIREBASE);
 
 server.use(Restify.jsonp());
 server.use(Restify.bodyParser());
-// server.use((req, res, next) => f.verifySignature(req, res, next));
+server.use((req, res, next) => f.verifySignature(req, res, next));
 
 const session = require('./session');
 const firebaseHelper = require('./firebase')(session, f);
