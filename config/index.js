5d0e6774b02ff30dd7c9a9ac398c2570d7dc6b44
ggomaeng <sungwoopark95@gmail.com>
2017-02-27 19:46:16 +0900

    update

--- config/index.js
+++ config/index.js
@@ -1,17 +1,17 @@
 'use strict';
 if(process.env.NODE_ENV === 'production') {
 	module.exports = {
-		WIT_ACCESS_TOKEN: process.env.WIT_ACCESS_TOKEN,
-		FB: {
-			PAGE_ACCESS_TOKEN: process.env.PAGE_ACCESS_TOKEN,
-			VERIFY_TOKEN: process.env.VERIFY_TOKEN,
-			APP_SECRET: process.env.APP_SECRET
+		"WIT_ACCESS_TOKEN": process.env.WIT_ACCESS_TOKEN,
+		"FB": {
+			"PAGE_ACCESS_TOKEN": process.env.PAGE_ACCESS_TOKEN,
+			"VERIFY_TOKEN": process.env.VERIFY_TOKEN,
+			"APP_SECRET": process.env.APP_SECRET
 		},
-		FIREBASE: {
+		"FIREBASE": {
 			apiKey: process.env.FIREBASE_API_KEY,
 			databaseURL: process.env.FIREBASE_DATABASE
 		},
-        SIMSIMI: process.env.SIMSIMI
+        "SIMSIMI": process.env.SIMSIMI
 	}
 } else {
 	module.exports = require('./development.json');
