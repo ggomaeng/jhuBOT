bc355b2eb3c951f3c63e3dbb7981f46ef41fc666
ggomaeng <sungwoopark95@gmail.com>
2017-02-27 19:47:31 +0900

    simsimi token

--- config/index.js
+++ config/index.js
@@ -5,13 +5,13 @@ if(process.env.NODE_ENV === 'production') {
 		"FB": {
 			"PAGE_ACCESS_TOKEN": process.env.PAGE_ACCESS_TOKEN,
 			"VERIFY_TOKEN": process.env.VERIFY_TOKEN,
-			"APP_SECRET": process.env.APP_SECRET
+			"APP_SECRET": process.env.APP_SECRET,
+            "SIMSIMI": process.env.SIMSIMI
 		},
 		"FIREBASE": {
 			apiKey: process.env.FIREBASE_API_KEY,
 			databaseURL: process.env.FIREBASE_DATABASE
-		},
-        "SIMSIMI": process.env.SIMSIMI
+		}
 	}
 } else {
 	module.exports = require('./development.json');
